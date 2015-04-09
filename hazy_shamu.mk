e7d2277c7dc5b521893d37ab645a4d217ebb805e
OhMyLob <matteolob1704@gmail.com>
2015-04-09 19:28:49 +0200

    Nexus 6 Support

---
+++ hazy_shamu.mk
@@ -0,0 +1,24 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1440
+TARGET_SCREEN_WIDTH := 2560
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/hazy/configs/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/hazy/configs/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/moto/shamu/aosp_shamu.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/moto/shamu/overlay-cm
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := hazy_shamu
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 6
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=shamu \
+    BUILD_FINGERPRINT=google/shamu/shamu:5.1/LMY47I/1767468:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 5.1 LMY47I 1767468 release-keys"
